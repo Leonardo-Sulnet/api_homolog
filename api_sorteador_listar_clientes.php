e26a1ab2fef345597bef082e2fab4e9067eb3df1
Elias Knebel - SULNET <elias.knebel@sulnet.com.br>
2024-11-04 10:54:32 -0300

    Update api_sorteador_listar_clientes.php

--- api_sorteador_listar_clientes.php
+++ api_sorteador_listar_clientes.php
@@ -49,7 +49,7 @@ if (validarTokenEAcesso($token, $apiPath, $conn_api)) {
    
 
     $sql = "SELECT ca.cd_lead as lead,  p.nome_razaosocial AS cliente, COALESCE(p.cpf, p.cnpj) AS documento, ca.codcontrato AS contrato,
-    'http://sulnet.net.br/sorteador/index.html?hash=' || 
+    'http://sulnet.net.br/sorteador/index.html?hash=' ||
     md5(replace(replace(replace(COALESCE(p.cpf, p.cnpj),'.', ''), '-', ''), ' ', '')) || ca.codcontrato::text AS hash,
     ca.descricao_plano AS descricao_plano,
         CASE
@@ -61,7 +61,7 @@ if (validarTokenEAcesso($token, $apiPath, $conn_api)) {
     r.nome AS nome_ganhador,
     r.id_premio AS id_premio,
     r.descricao_premio AS premio,
-to_char(ca.dt_ativacao,'YYYY-MM-DD') AS ativacao,
+    to_char(ca.dt_ativacao,'YYYY-MM-DD') AS ativacao,
     TO_CHAR(r.dt_hora_insert, 'YYYY-MM-DD') AS data_insert
 FROM
     mk_pessoas p
@@ -92,31 +92,33 @@ LEFT JOIN (
             END
         ) = 1
 ) AS ca ON p.codpessoa = ca.cliente
-LEFT JOIN vi_bi_crm ON vi_bi_crm.codigo_lead = ca.cd_lead
+
+LEFT JOIN mk_crm_leads on mk_crm_leadS.codlead = ca.cd_lead
 LEFT JOIN (
-    SELECT 
-        id_ganhador, 
-        nome, 
-        documento, 
-        contrato, 
-        documento_contrato_hash, 
+    SELECT
+        id_ganhador,
+        nome,
+        documento,
+        contrato,
+        documento_contrato_hash,
         id_premio,
         descricao_premio,
         dt_hora_insert
-    FROM 
-        dblink('host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD',
+    FROM
+    dblink('host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD',
                 'SELECT id_ganhador, nome, documento, contrato, documento_contrato_hash, ganhador.id_premio, descricao_premio, dt_hora_insert
-                 FROM ganhador  
+                 FROM ganhador
                  LEFT JOIN premios ON premios.id_premio = ganhador.id_premio
                 '
-                ) 
+                )
     AS roleta_data(id_ganhador INT, nome TEXT, documento TEXT, contrato TEXT, documento_contrato_hash TEXT, id_premio INT, descricao_premio TEXT, dt_hora_insert DATE)
 ) AS r ON r.contrato = ca.codcontrato
 WHERE
     ca.contrato_ativo = 1
-    --AND vi_bi_crm.VENDA_CRM IN ('REATIVA','ATIVA')
+    AND venda_crm in (1,2)
     AND ca.codplano IN (833, 1322,1330,1492,1501,1462,1458,1457,1456,1468,1467,1469,1501,1492,1330,1368)
-    AND DATE_TRUNC('day', ca.dt_ativacao) BETWEEN DATE_TRUNC('day', '$pi'::date) AND DATE_TRUNC('day', '$pf'::date)
+
+AND DATE_TRUNC('day', ca.dt_ativacao) BETWEEN DATE_TRUNC('day', '$pi'::date) AND DATE_TRUNC('day', '$pf'::date)
 ORDER BY
   ca.dt_ativacao DESC";
  //   echo $sql;
