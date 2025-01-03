4598eab577ae5d1a0ef55445752029e4a5679ec9
Elias Knebel - SULNET <elias.knebel@sulnet.com.br>
2025-01-03 11:39:01 -0300

    api

--- conexao.php
+++ conexao.php
@@ -5,13 +5,11 @@ $usuario = "cliente_r";
 $senha ="Cl13nt_R";
 
 try {
-
 $conn = new PDO("pgsql:host=187.109.17.5 dbname=mkData3.0 user=cliente_r password=Cl13nt_R");
 $conn->exec("SET NAMES 'utf8'");
 } catch (PDOException $e) {
     die($e->getMessage());
 }
-   //echo "conexão BD ok";
 
 
 //Acessso ao servidor de API
