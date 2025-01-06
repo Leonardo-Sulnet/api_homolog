f19572523ac3abf86099e2598b1b221a48c1bd5e
Elias Knebel - SULNET <elias.knebel@sulnet.com.br>
2025-01-06 11:19:27 -0300

    Update api_mail_queue_generator.php

--- api_mail_queue_generator.php
+++ api_mail_queue_generator.php
@@ -13,6 +13,7 @@ $headers = getallheaders();
 
 // Simulação de um endpoint de API recebendo um token no cabeçalho e o caminho de API
 $token = isset($headers['Authorization']) ? $headers['Authorization'] : null;
+$token = isset($headers['authorization']) ? $headers['authorization'] : null;
 $apiPath = !empty($_SERVER['PHP_SELF']) ? basename($_SERVER['PHP_SELF']) : null;
 
 
